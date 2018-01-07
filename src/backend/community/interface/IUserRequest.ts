07e6cf4e332ea2c61fc90585085d38b5fefdaf1a
matthewpoletin <contact@matthewpoletin.ru>
2018-01-07 14:21:46 +0300

    Avatar and bio of user added

--- src/backend/community/interface/IUserRequest.ts
+++ src/backend/community/interface/IUserRequest.ts
@@ -2,10 +2,13 @@
 
 export default interface IUserRequest {
     username?: string;
+    avatar?: string;
+    bio?: string;
     forename?: string;
     surname?: string;
     regEmail?: string;
     pubEmail?: string;
     ghLink?: string;
     fbLink?: string;
+    password?: string;
 }
--- src/backend/community/interface/IUserResponse.ts
+++ src/backend/community/interface/IUserResponse.ts
@@ -5,12 +5,15 @@ import IPaginated from "../../IPaginated";
 export default interface IUserResponse {
     id: number;
     username?: string;
+    avatar?: string;
+    bio?: string;
     forename?: string;
     surname?: string;
     regEmail?: string;
     pubEmail?: string;
     ghLink?: string;
     fbLink?: string;
+    password?: string;
 }
 
 export interface IUserPaginated extends IPaginated {
